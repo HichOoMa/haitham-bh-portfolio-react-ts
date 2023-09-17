b46f3431d92a5691eee257c31b0c1d8681126703
HaithamBH <haitham.benhammouda@gmail.com>
2023-09-17 20:31:05 +0100

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import Home from './components/home/home.tsx'
+
+import './App.css'
+import Layout from './components/layout/layout.tsx'
+
+function App() {
+
+  return (
+    <>
+      <Layout>
+        <Home />
+      </Layout>
+    </>
+  )
+}
+
+export default App
