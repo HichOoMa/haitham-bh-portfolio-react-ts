d292e22b21c35b31a48818c0240eb0c46df5b0ad
HaithamBH <haitham.benhammouda@gmail.com>
2023-09-20 00:04:32 +0100

    setup new component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
 import Home from './components/home/home.tsx'
+import About from './components/about/about.tsx'
+import Navbar from './components/navbar/navbar.tsx'
+import Projects from './components/projects/projects.tsx'
 
 import './App.css'
-import Layout from './components/layout/layout.tsx'
+import Contact from './components/contact/contact.tsx'
 
 function App() {
 
   return (
     <>
-      <Layout>
-        <Home />
-      </Layout>
+      <div>
+        <Navbar />
+      </div>
+      <main style={{display: 'flex', flexDirection: 'column'}}>
+          <Home />
+          <About />
+          <Projects />
+          <Contact />
+      </main>
     </>
   )
 }
