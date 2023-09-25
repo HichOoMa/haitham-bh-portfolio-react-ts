ac08fc80aa49df1672a3bc1807cf5b042eedd64b
HaithamBH <haitham.benhammouda@gmail.com>
2023-09-25 13:26:47 +0100

    contact page

---
+++ src/components/projects/projects.tsx
@@ -0,0 +1,104 @@
+import ProjectCard from '../projectCard/projectCard';
+import './projects.css'
+
+const Projects = () => {
+  return(
+    <section className='projects'>
+      <span className="section-title">PROJECTS</span>
+      <div className="projects-list">
+        <ProjectCard 
+          name='Test Project' 
+          imgUrl='./react.png' 
+          description='Vestibulum rutrum iaculis pellentesque. Nam sed nisl ex. 
+          Maecenas faucibus eleifend sapien, a lobortis diam accumsan quis. Proin 
+          sed feugiat lacus. Vestibulum ante ipsum primis in faucibus orci luctus 
+          et ultrices posuere cubilia curae; Mauris bibendum gravida nulla, ac 
+          elementum neque bibendum vitae. Sed a ornare sem. Suspendisse ut venenatis 
+          est. Pellentesque interdum luctus lorem sit amet fermentum.' 
+        />
+        <ProjectCard 
+          name='Test Project' 
+          imgUrl='./react.png' 
+          description='Vestibulum rutrum iaculis pellentesque. Nam sed nisl ex. 
+          Maecenas faucibus eleifend sapien, a lobortis diam accumsan quis. Proin 
+          sed feugiat lacus. Vestibulum ante ipsum primis in faucibus orci luctus 
+          et ultrices posuere cubilia curae; Mauris bibendum gravida nulla, ac 
+          elementum neque bibendum vitae. Sed a ornare sem. Suspendisse ut venenatis 
+          est. Pellentesque interdum luctus lorem sit amet fermentum.' 
+        />
+        <ProjectCard 
+          name='Test Project' 
+          imgUrl='./react.png' 
+          description='Vestibulum rutrum iaculis pellentesque. Nam sed nisl ex. 
+          Maecenas faucibus eleifend sapien, a lobortis diam accumsan quis. Proin 
+          sed feugiat lacus. Vestibulum ante ipsum primis in faucibus orci luctus 
+          et ultrices posuere cubilia curae; Mauris bibendum gravida nulla, ac 
+          elementum neque bibendum vitae. Sed a ornare sem. Suspendisse ut venenatis 
+          est. Pellentesque interdum luctus lorem sit amet fermentum.' 
+        />
+        <ProjectCard 
+          name='Test Project' 
+          imgUrl='./react.png' 
+          description='Vestibulum rutrum iaculis pellentesque. Nam sed nisl ex. 
+          Maecenas faucibus eleifend sapien, a lobortis diam accumsan quis. Proin 
+          sed feugiat lacus. Vestibulum ante ipsum primis in faucibus orci luctus 
+          et ultrices posuere cubilia curae; Mauris bibendum gravida nulla, ac 
+          elementum neque bibendum vitae. Sed a ornare sem. Suspendisse ut venenatis 
+          est. Pellentesque interdum luctus lorem sit amet fermentum.' 
+        />
+        <ProjectCard 
+          name='Test Project' 
+          imgUrl='./react.png' 
+          description='Vestibulum rutrum iaculis pellentesque. Nam sed nisl ex. 
+          Maecenas faucibus eleifend sapien, a lobortis diam accumsan quis. Proin 
+          sed feugiat lacus. Vestibulum ante ipsum primis in faucibus orci luctus 
+          et ultrices posuere cubilia curae; Mauris bibendum gravida nulla, ac 
+          elementum neque bibendum vitae. Sed a ornare sem. Suspendisse ut venenatis 
+          est. Pellentesque interdum luctus lorem sit amet fermentum.' 
+        />
+        <ProjectCard 
+          name='Test Project' 
+          imgUrl='./react.png' 
+          description='Vestibulum rutrum iaculis pellentesque. Nam sed nisl ex. 
+          Maecenas faucibus eleifend sapien, a lobortis diam accumsan quis. Proin 
+          sed feugiat lacus. Vestibulum ante ipsum primis in faucibus orci luctus 
+          et ultrices posuere cubilia curae; Mauris bibendum gravida nulla, ac 
+          elementum neque bibendum vitae. Sed a ornare sem. Suspendisse ut venenatis 
+          est. Pellentesque interdum luctus lorem sit amet fermentum.' 
+        />
+        <ProjectCard 
+          name='Test Project' 
+          imgUrl='./react.png' 
+          description='Vestibulum rutrum iaculis pellentesque. Nam sed nisl ex. 
+          Maecenas faucibus eleifend sapien, a lobortis diam accumsan quis. Proin 
+          sed feugiat lacus. Vestibulum ante ipsum primis in faucibus orci luctus 
+          et ultrices posuere cubilia curae; Mauris bibendum gravida nulla, ac 
+          elementum neque bibendum vitae. Sed a ornare sem. Suspendisse ut venenatis 
+          est. Pellentesque interdum luctus lorem sit amet fermentum.' 
+        />
+        <ProjectCard 
+          name='Test Project' 
+          imgUrl='./react.png' 
+          description='Vestibulum rutrum iaculis pellentesque. Nam sed nisl ex. 
+          Maecenas faucibus eleifend sapien, a lobortis diam accumsan quis. Proin 
+          sed feugiat lacus. Vestibulum ante ipsum primis in faucibus orci luctus 
+          et ultrices posuere cubilia curae; Mauris bibendum gravida nulla, ac 
+          elementum neque bibendum vitae. Sed a ornare sem. Suspendisse ut venenatis 
+          est. Pellentesque interdum luctus lorem sit amet fermentum.' 
+        />
+        <ProjectCard 
+          name='Test Project' 
+          imgUrl='./react.png' 
+          description='Vestibulum rutrum iaculis pellentesque. Nam sed nisl ex. 
+          Maecenas faucibus eleifend sapien, a lobortis diam accumsan quis. Proin 
+          sed feugiat lacus. Vestibulum ante ipsum primis in faucibus orci luctus 
+          et ultrices posuere cubilia curae; Mauris bibendum gravida nulla, ac 
+          elementum neque bibendum vitae. Sed a ornare sem. Suspendisse ut venenatis 
+          est. Pellentesque interdum luctus lorem sit amet fermentum.' 
+        />
+      </div>
+    </section>
+  )
+}
+
+export default Projects;
